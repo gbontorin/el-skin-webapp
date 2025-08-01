a8a9b90cd2fcf0c5b8df65a4b377c0652f5c1d81
Guilherme <gbontorin@yahoo.fr>
2025-08-01 15:04:50 -0300

    Desafio S4 ThemeProvider

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 import 'normalize.css';
  
+
 export const GlobalStyles = createGlobalStyle`
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
